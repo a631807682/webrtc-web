dc085464dac62a9c06643ed49f6d7e7c9adf4a2c
a631807682 <631807682@qq.com>
2019-04-04 15:59:59 +0800

    refactor: Update ice server config

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,7 +3,7 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  WS_HOST: '"http://127.0.0.1:3001"',
+  WS_HOST: '"https://127.0.0.1:3001"',
   STUN_HOST: '"stun:192.168.20.209:5349"',
   TURN_HOST: '"turn:192.168.20.209:3478"',
   TURN_username: '"mytest"',
